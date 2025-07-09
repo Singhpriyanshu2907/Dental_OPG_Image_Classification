b5e71db544a80c9fe80da0c14f540398804098ce
Singhpriyanshu2907 <priyanshus2907@gmail.com>
2025-07-09 19:25:46 +0530

    Structure setup

---
+++ project_structure.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+
+
+list_of_files = [
+    "github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/custom_exception.py",
+    "src/logger.py",
+    "artifacts/",  
+    "config/__init__.py",  
+    "notebooks/experiments.ipynb",
+    "static/",
+    "templates/",
+    "utils/__init__.py",
+    "requirements.txt"
+]
+
+
+for filepath in list_of_files:
+    filepath = pathlib.Path(filepath)
+
+    if filepath.suffix:
+        filedir = filepath.parent
+
+        if filedir and not filedir.exists():
+            os.makedirs(filedir, exist_ok = True)
+
+        
+        if not filepath.exists():
+            with open(filepath, "w") as f:
+                pass
+            print(f"Created empty file at: {filepath} ")
+        else:
+            if not filepath.exists():
+                os.makedirs(filepath, exist_ok = True)
+                print(f"Created directory: {filepath}")
